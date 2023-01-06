509df579c1f39ba2741fd751188921ebb45b993d
Andrii Marchuk <andrii.marchuck@gmail.com>
2023-01-06 16:29:06 +0200

    add 2 and 3 tasks

---
+++ src/js/03-promises.js
@@ -0,0 +1,40 @@
+import Notiflix from 'notiflix';
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const createBtn = document.querySelector('button[type="submit"]');
+const amountValue = document.querySelector('input[name="amount"]');
+
+
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({ position, delay });;
+      }
+    }, delay);
+  });
+  return promise
+}
+
+createBtn.addEventListener('click', e => {
+  e.preventDefault();
+  let firstDelay = Number(delay.value);
+  let delayStep = Number(step.value);
+  for (let i = 0; i < amountValue.value; i++) {
+    createPromise(1 + i, firstDelay + i * delayStep)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
